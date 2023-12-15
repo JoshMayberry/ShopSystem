e6859fc901ca46a172ba6b359ea89f0d53d41644
HOME_PC\JoshuaMayberry <joshua.mayberry1991@gmail.com>
2023-12-14 21:07:53 -0700

    Split file into separate files. Added example use.

--- Runtime/ResourceManager.cs
+++ Runtime/ResourceManager.cs
@@ -1,21 +1,42 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace jmayberry.Shop
-{
-    public class ResourceManager : MonoBehaviour
-    {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
+using AYellowpaper.SerializedCollections;
+
+namespace jmayberry.Shop {
+    public class ResourceManager : MonoBehaviour {
+        public static ResourceManager instance { get; private set; }
+
+        private void Awake() {
+            if (instance != null) {
+                Debug.LogError("Found more than one BuildManager in the scene.");
+            }
+            instance = this;
         }
 
-        // Update is called once per frame
-        void Update()
-        {
-        
+        [SerializedDictionary("Resource", "Amount")]
+        public SerializedDictionary<Resource, int> resources;
+
+        public void AddResource(Resource resource, int amount) {
+            if (!resources.ContainsKey(resource)) {
+                resources[resource] = 0;
+            }
+
+            resources[resource] += amount;
+        }
+
+        public bool ConsumeResources(List<ResourceAmount> costs) {
+            foreach (var cost in costs) {
+                if (!resources.ContainsKey(cost.resource) || resources[cost.resource] < cost.amount)
+                    return false; // Not enough resources
+            }
+
+            foreach (var cost in costs) {
+                resources[cost.resource] -= cost.amount;
+            }
+
+            return true;
         }
     }
 }
